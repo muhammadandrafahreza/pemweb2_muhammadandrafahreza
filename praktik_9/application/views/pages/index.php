b0448a5f771b7de0a0688677dccfa5f91935465b
muhammadandrafahreza <93003664+muhammadandrafahreza@users.noreply.github.com>
2022-07-03 22:48:35 +0700

    Add files via upload

---
+++ praktik_9/application/views/pages/index.php
@@ -0,0 +1,11 @@
+<div class="container mt-4">
+    <div class="row ">
+        <div class="col">
+            <h2 class="display-6">Praktikum 9 - Login + CRUD</h2>
+            <hr />
+            <p class="text-center m-0">Muhammad Andra Fahreza</p>
+            <p class="text-center m-0">0110221203</p>
+            <p class="text-center m-0">TI18</p>
+        </div>
+    </div>
+</div>

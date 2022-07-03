4f83e4a0bdf767d6ff24145a58237cb083426468
muhammadandrafahreza <93003664+muhammadandrafahreza@users.noreply.github.com>
2022-07-03 21:25:58 +0700

    Add files via upload

---
+++ praktik_6/application/views/pages/index.php
@@ -0,0 +1,7 @@
+<div class="cantainer mt-4 text-center">
+    <h1 class="display-6">Praktikum 6</h1>
+    <div class="mt-2">
+        <p class="m-0">Muhammad Andra Fahreza</p>
+        <p class="m-0">TI18</p>
+    </div>
+</div>
